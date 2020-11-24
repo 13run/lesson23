b84820a084cd28114ebf829263114381195e7fd4
13Run <runemal13@gmail.com>
2020-11-24 15:30:03 +0500

    ver1

--- app.rb
+++ app.rb
@@ -1,47 +1,45 @@
-#encoding: utf-8
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+  erb 'Hello! <a href="https://github.com/bootstrap-ruby/sinatra-bootstrap">Original</a> pattern has been modified for <a href="http://rubyschool.us/">Ruby School</a>'
 end
 
-get '/about' do 
-	erb :about
+get '/about' do
+  erb :about
 end
 
-get '/visits' do 
-	erb :visits
+get '/visits' do
+  erb :visits
 end
 
-post '/visits' do 
-	@user = params[:user_name]
-	@phone = params[:user_phone]
-	@date = params[:date_time]
-	@barber = params[:barber]
+post '/visits' do
+  @user = params[:user_name]
+  @phone = params[:user_phone]
+  @date = params[:date_time]
+  @barber = params[:barber]
 
-	@f = File.open('./public/users.txt', 'a')
-	@f.write "User: [#{@user}] -- Phone: [#{@phone}] -- Date time: [#{@date}] -- Barber: [#{@barber}]\n"
-	@f.close
+  @f = File.open('./public/users.txt', 'a')
+  @f.write "User: [#{@user}] -- Phone: [#{@phone}] -- Date time: [#{@date}] -- Barber: [#{@barber}]\n"
+  @f.close
 
-	@title = "Dear, #{@user}!"
-	@message = " We'll wait you at #{@date}. Your barber - #{@barber}."
-
-	erb :message
+  erb "Dear, #{@user}! We'll wait you at #{@date}. Your barber - #{@barber}."
 end
 
-get '/contacts' do 
-	erb :contacts
+get '/contacts' do
+  erb :contacts
 end
 
-post '/contacts' do 
-	@user_mail = params[:user_email]
-	@user_msg = params[:user_message]
+post '/contacts' do
+  @user_mail = params[:user_email]
+  @user_msg = params[:user_message]
 
-	@f = File.open('./public/contacts.txt', 'a')
-	@f.write "User mail: [#{@user_mail}] -- Message: [#{@user_msg}]\n"
-	@f.close
+  @f = File.open('./public/contacts.txt', 'a')
+  @f.write "User mail: [#{@user_mail}] -- Message: [#{@user_msg}]\n"
+  @f.close
 
-	erb :contacts
+  erb :contacts
 end
